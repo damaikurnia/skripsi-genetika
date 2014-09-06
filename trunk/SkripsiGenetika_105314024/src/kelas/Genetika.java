bf97298ceadc94c26b12799a0f03ef76c3cd2857
black.paradekid@gmail.com <black.paradekid@gmail.com@6532ed8a-7f0f-730e-353b-e07f87f7d8ac>
2014-09-06 01:01:52 +0000

    mulai mutasi 2

--- trunk/SkripsiGenetika_105314024/src/kelas/Genetika.java
+++ trunk/SkripsiGenetika_105314024/src/kelas/Genetika.java
@@ -5,11 +5,25 @@
  */
 package kelas;
 
+import java.sql.SQLException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import kontrol.KelasMatkulKontrol;
+import kontrol.MataKuliahKontrol;
+
 /**
  *
  * @author Adhi
  */
 public class Genetika {
+    List<KelasKuliah> kelasKuliah;
+    List<MataKuliah> matkul;
+    String[] checklist_kk; // <-- untuk cek kk sdh ada di dlm kromosom apa blm.
+    
+    public Genetika(){
+        ambilDatabase();
+    }
 //        public void routleWheelSelection() {
 ////        for (int i = 0; i < parent.length; i++) {
 ////            Kromosom kromosom = parent[i];
@@ -33,32 +47,61 @@ public class Genetika {
 
     public static Kromosom[] crossover(Kromosom[] parent) {
 //        routleWheelSelection();
-        Kromosom[] popul = parent;
+        Kromosom[] krom = parent;
 //        popul[0] = parent1;//parent 1 A - B
 //        popul[1] = parent2;//parent 2 C - D
 //        popul[2] = parent1;//child 1 A - D
 //        popul[3] = parent2;//child 2 C - B
         
-        int point = popul[0].getData().length/2;//titik one point crossover
-        for (int i = 0; i < popul[0].getData().length; i++) {
+        int point = krom[0].getData().length/2;//titik one point crossover
+        for (int i = 0; i < krom[0].getData().length; i++) {
             if (i<point) { // samakan gen A dan C ke child 1 dan 2
-                popul[2].getData()[i].setAllele(popul[0].getData()[i].getAllele());
-                popul[3].getData()[i].setAllele(popul[1].getData()[i].getAllele());
+                krom[2].getData()[i].setAllele(krom[0].getData()[i].getAllele());
+                krom[3].getData()[i].setAllele(krom[1].getData()[i].getAllele());
             }
             else{
-                popul[2].getData()[i].setAllele(popul[1].getData()[i].getAllele());
-                popul[3].getData()[i].setAllele(popul[0].getData()[i].getAllele());
+                krom[2].getData()[i].setAllele(krom[1].getData()[i].getAllele());
+                krom[3].getData()[i].setAllele(krom[0].getData()[i].getAllele());
             }
-            popul[2].getData()[i].setNilaiFitness(0);
-            popul[3].getData()[i].setNilaiFitness(0);
+            krom[2].getData()[i].setNilaiFitness(0);
+            krom[3].getData()[i].setNilaiFitness(0);
         }
         
-        return popul;
+        return krom;
     }
 
     public static Kromosom[] Mutasi(Kromosom[] parent) {
+        Kromosom[] krom = parent;
         
+        //replace id duplikat (setelah di cross)
+        for (int i = 2; i < 4; i++) { // 2-3 --> hanya kromosom anak yg di mutasi
+            for (int j = 0; j < krom[i].data.length; j++) {
+                if(krom[i].data[j].allele.getIdKelas() != 0){
+                    //<<-sampe disini
+                }
+            }
+        }
+        
+        //pindahkah 1 gen
         return null;
     }
 
+    public void ambilDatabase(){
+        try {
+            kelasKuliah = KelasMatkulKontrol.getKoneksi().tampilKelasMataKuliah();
+            matkul = MataKuliahKontrol.getKoneksi().tampilMataKuliah();
+            checklist_kk = new String[kelasKuliah.size()];
+            //addOn checklist_kk
+            for (int i = 0; i < kelasKuliah.size(); i++) {
+                checklist_kk[i] = Integer.toString(kelasKuliah.get(i).getIdKelas())+"-0";
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Genetika.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    
+//    public static void main(String[] args) {
+//        Genetika a = new Genetika();
+//    }
 }
