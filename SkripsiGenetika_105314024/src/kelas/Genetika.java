2e05f737aabc1c405d5b0ae7d8de97d042713e52
black.paradekid@gmail.com <black.paradekid@gmail.com@6532ed8a-7f0f-730e-353b-e07f87f7d8ac>
2014-09-08 14:19:38 +0000

    sercing sorting

--- SkripsiGenetika_105314024/src/kelas/Genetika.java
+++ SkripsiGenetika_105314024/src/kelas/Genetika.java
@@ -17,11 +17,12 @@ import kontrol.MataKuliahKontrol;
  * @author Adhi
  */
 public class Genetika {
+
     List<KelasKuliah> kelasKuliah;
     List<MataKuliah> matkul;
     String[] checklist_kk; // <-- untuk cek kk sdh ada di dlm kromosom apa blm.
-    
-    public Genetika(){
+
+    public Genetika() {
         ambilDatabase();
     }
 //        public void routleWheelSelection() {
@@ -52,56 +53,90 @@ public class Genetika {
 //        popul[1] = parent2;//parent 2 C - D
 //        popul[2] = parent1;//child 1 A - D
 //        popul[3] = parent2;//child 2 C - B
-        
-        int point = krom[0].getData().length/2;//titik one point crossover
+
+        int point = krom[0].getData().length / 2;//titik one point crossover
         for (int i = 0; i < krom[0].getData().length; i++) {
-            if (i<point) { // samakan gen A dan C ke child 1 dan 2
+            if (i < point) { // samakan gen A dan C ke child 1 dan 2
                 krom[2].getData()[i].setAllele(krom[0].getData()[i].getAllele());
                 krom[3].getData()[i].setAllele(krom[1].getData()[i].getAllele());
-            }
-            else{
+            } else {
                 krom[2].getData()[i].setAllele(krom[1].getData()[i].getAllele());
                 krom[3].getData()[i].setAllele(krom[0].getData()[i].getAllele());
             }
             krom[2].getData()[i].setNilaiFitness(0);
             krom[3].getData()[i].setNilaiFitness(0);
         }
-        
+
         return krom;
     }
 
     public static Kromosom[] Mutasi(Kromosom[] parent) {
         Kromosom[] krom = parent;
-        
+
         //replace id duplikat (setelah di cross)
-        for (int i = 2; i < 4; i++) { // 2-3 --> hanya kromosom anak yg di mutasi
+        for (int i = 2; i < krom.length; i++) { // 2-3 --> hanya kromosom anak yg di mutasi
             for (int j = 0; j < krom[i].data.length; j++) {
-                if(krom[i].data[j].allele.getIdKelas() != 0){
-                    //<<-sampe disini
+                if (krom[i].data[j].allele.getIdKelas() != 0) {
+//                    if () {
+//                    }
+//<<-sampe disini
                 }
             }
         }
-        
+
         //pindahkah 1 gen
         return null;
     }
 
-    public void ambilDatabase(){
+    public void ambilDatabase() {
         try {
             kelasKuliah = KelasMatkulKontrol.getKoneksi().tampilKelasMataKuliah();
             matkul = MataKuliahKontrol.getKoneksi().tampilMataKuliah();
             checklist_kk = new String[kelasKuliah.size()];
             //addOn checklist_kk
             for (int i = 0; i < kelasKuliah.size(); i++) {
-                checklist_kk[i] = Integer.toString(kelasKuliah.get(i).getIdKelas())+"-0";
+                checklist_kk[i] = Integer.toString(kelasKuliah.get(i).getIdKelas()) + "-0";
             }
+            sorting();
         } catch (SQLException ex) {
             Logger.getLogger(Genetika.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    
-//    public static void main(String[] args) {
-//        Genetika a = new Genetika();
-//    }
+
+    public void sorting() { //<--insertion sort method
+        for (int i = 1; i <= checklist_kk.length - 1; i++) {
+            for (int j = i; j >= 1; j--) {
+                int banding1 = Integer.parseInt(checklist_kk[j].split("-")[0]);
+                int banding2 = Integer.parseInt(checklist_kk[j - 1].split("-")[0]);
+                if (banding1 < banding2) {
+                    String temp = checklist_kk[j];
+                    checklist_kk[j] = checklist_kk[j - 1];
+                    checklist_kk[j - 1] = temp;
+                }
+            }
+        }
+    }
+
+    public int sercing(int key) {
+        int awal = 0;
+        int akhir = checklist_kk.length - 1;
+        while (awal < akhir) {
+            int tengah = (awal + akhir) / 2;
+            int banding = Integer.parseInt(checklist_kk[tengah].split("-")[0]);
+            if (banding == key) {
+                return tengah;
+            } else {
+                if (banding > key) {
+                    akhir = tengah - 1;
+                } else {
+                    awal = tengah + 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Genetika a = new Genetika();
+    }
 }
