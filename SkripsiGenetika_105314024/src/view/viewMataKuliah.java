c58d7a999654f286e09d4cae5a89f47d36501aea
black.paradekid@gmail.com <black.paradekid@gmail.com@6532ed8a-7f0f-730e-353b-e07f87f7d8ac>
2014-10-30 12:41:04 +0000

    Ganti warna lagi

--- SkripsiGenetika_105314024/src/view/viewMataKuliah.java
+++ SkripsiGenetika_105314024/src/view/viewMataKuliah.java
@@ -78,7 +78,7 @@ public class viewMataKuliah extends javax.swing.JFrame {
         tabelMatakuliah.getColumnModel().getColumn(4).setMinWidth(30);
         tabelMatakuliah.getColumnModel().getColumn(4).setMaxWidth(30);
         
-        tabelMatakuliah.setDefaultRenderer(Object.class, new renderWarnaWarni(Color.gray, Color.white));
+        tabelMatakuliah.setDefaultRenderer(Object.class, new renderWarnaWarni(Color.lightGray, Color.white));
     }
     /** This method is called from within the constructor to
      * initialize the form.
@@ -276,7 +276,7 @@ public class viewMataKuliah extends javax.swing.JFrame {
 
         jPanel1.add(jPanel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 210, 940, 320));
 
-        jPanel5.setBackground(new java.awt.Color(0, 51, 204));
+        jPanel5.setBackground(new java.awt.Color(51, 102, 255));
         jPanel5.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jLabel6.setFont(new java.awt.Font("Wide Latin", 1, 19));
@@ -300,7 +300,7 @@ public class viewMataKuliah extends javax.swing.JFrame {
 
         jPanel1.add(jPanel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 940, 150));
 
-        jPanel2.setBackground(new java.awt.Color(0, 51, 204));
+        jPanel2.setBackground(new java.awt.Color(51, 102, 255));
         jPanel2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
         jPanel1.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 530, 940, 80));
 
